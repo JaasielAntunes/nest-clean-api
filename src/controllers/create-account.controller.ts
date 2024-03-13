0a1fba0e95ce38707d56d906a93491ba9840db6d
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-13 17:21:20 -0300

    Controller de criação de conta

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma-service';
+
+@Controller('/accounts')
+export class CreateAccountController {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private prisma: PrismaService) { }
+
+  @Post()
+  @HttpCode(201)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async handle(@Body() body: any) {
+    const { name, email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'Já existe um usuário com o mesmo endereço de e-mail!',
+      );
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+  }
+}
