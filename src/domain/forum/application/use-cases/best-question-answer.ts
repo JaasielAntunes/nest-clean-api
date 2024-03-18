aae02e8cef6b7cb86f70543bdfd9995b6ab6ea0a
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-18 16:04:32 -0300

    Controller e teste para escolher melhor resposta

--- src/domain/forum/application/use-cases/best-question-answer.ts
+++ src/domain/forum/application/use-cases/best-question-answer.ts
@@ -1,11 +1,10 @@
-/* eslint-disable prettier/prettier */
-import { AnswersRepository } from "../repositories/answers-repository";
-import { Question } from "@/domain/forum/enterprise/entities/question";
-import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
-import { Either, left, right } from "@/core/either";
-import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
-import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
-
+import { AnswersRepository } from '../repositories/answers-repository';
+import { Question } from '@/domain/forum/enterprise/entities/question';
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
+import { Either, left, right } from '@/core/either';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+import { Injectable } from '@nestjs/common';
 
 interface ChooseQuestionBestAnswerUseCaseRequest {
   authorId: string;
@@ -19,6 +18,7 @@ type ChooseQuestionBestAnswerUseCaseResponse = Either<
   }
 >;
 
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
