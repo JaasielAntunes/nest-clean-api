4a1389b8b3d3fa9816a38df7e932cbf5be81ecb1
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-18 16:18:36 -0300

    Controller e teste para deletar comentário da resposta

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,9 +1,8 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/ban-types */
-import { Either, left, right } from "@/core/either";
-import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
-import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
-import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
+import { Either, left, right } from '@/core/either';
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { Injectable } from '@nestjs/common';
 
 interface DeleteAnswerCommentUseCaseRequest {
   authorId: string;
@@ -15,6 +14,7 @@ type DeleteAnswerCommentUseCaseResponse = Either<
   {}
 >;
 
+@Injectable()
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) { }
 
