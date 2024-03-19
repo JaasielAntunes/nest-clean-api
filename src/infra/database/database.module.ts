8fe2f2607f02791765732bcc2b6dbc407babaaa8
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-03-19 11:36:33 -0300

    Teste do controller de upload

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -14,6 +14,8 @@ import { AnswerCommentsRepository } from '@/domain/forum/application/repositorie
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository';
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository';
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository';
+import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository';
+import { PrismaAttachmentsRepository } from './prisma/repositories/prisma-attachments-repository';
 
 @Module({
   providers: [
@@ -25,6 +27,10 @@ import { QuestionCommentsRepository } from '@/domain/forum/application/repositor
     {
       provide: StudentsRepository,
       useClass: PrismaStudentsRepository,
+    },
+    {
+      provide: AttachmentsRepository,
+      useClass: PrismaAttachmentsRepository,
     },
     {
       provide: QuestionCommentsRepository,
@@ -56,6 +62,7 @@ import { QuestionCommentsRepository } from '@/domain/forum/application/repositor
     AnswersRepository,
     AnswerCommentsRepository,
     AnswerAttachmentsRepository,
+    AttachmentsRepository,
   ],
 })
 export class DatabaseModule { }
